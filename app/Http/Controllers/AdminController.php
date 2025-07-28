cc26651222c8cea6b21734546841dcb57c686857
manaja13 <maulanaaji693@gmail.com>
2025-07-28 11:12:52 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -169,7 +169,6 @@ class AdminController extends Controller
         // buat pengecekan setiap users yang akan dihapus cek dulu dia punya gambar atau tidak?, jika ada maka hapus gambar , jika tidak ada langsung delte saja //
         
         use Illuminate\Support\Facades\File;
-
 public function destroy($id)
 {
     $user = User::findOrFail($id);
